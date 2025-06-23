bdb45d7d8dde235010b44eb16f8f93ca652839ef
Prem Prajapat <100069093+Premkumarprajapat@users.noreply.github.com>
2025-06-23 21:48:17 +0530

    Update train_mask_model.py

--- train_mask_model.py
+++ train_mask_model.py
@@ -1,52 +1,46 @@
-import os
+import os 
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
-
+from tensorflow.keras.layers import Dense,GlobalAveragePooling2D
 
 img_size = 224
 batch_size = 32
 
 train_datagen = ImageDataGenerator(
-    rescale=1./255,
+    rescale = 1./255,
     validation_split=0.2,
-    zoom_range=0.2,
-    shear_range=0.2,
-    horizontal_flip=True
+    horizontal_flip = True
 )
 
 train_generator = train_datagen.flow_from_directory(
-    'dataset/',                  
-    target_size=(img_size, img_size),
+    'dataset/',
+    target_size=(img_size,img_size),
     batch_size=batch_size,
-    class_mode='categorical',   
+    class_mode='categorical',
     subset='training'
 )
 
 val_generator = train_datagen.flow_from_directory(
     'dataset/',
-    target_size=(img_size, img_size),
+    target_size=(img_size,img_size),
     batch_size=batch_size,
     class_mode='categorical',
     subset='validation'
 )
 
+base_model = MobileNetV2(weights='imagenet',include_top=False,input_shape=(img_size,img_size,3))
+base_model.trainable = False
 
-base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-base_model.trainable = False  # freeze pre-trained layers
+x=base_model.output
+x=GlobalAveragePooling2D()(x)
+x=Dense(128,activation='relu')(x)
+output = Dense(2,activation='softmax')(x)
 
-x = base_model.output
-x = GlobalAveragePooling2D()(x)
-x = Dense(128, activation='relu')(x)
-output = Dense(2, activation='softmax')(x)  # 2 classes: with_mask, without_mask
+model = Model(imputs=base_model.input,outputs=output)
+model.compile(optimizer='adam',loss='categorial_crossentropy',metrics=['accuracy'])
 
-model = Model(inputs=base_model.input, outputs=output)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_generator,validation_data=val_generator,epochs=10)
 
-
-model.fit(train_generator, validation_data=val_generator, epochs=10)
-
-model.save("")
-print("✅ Model saved as mask_detector_model.h5")
+model.save("mask_detector_model.h5")
